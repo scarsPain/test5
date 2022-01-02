51d343a290df8a78592d71458b4f348349654706
scarsPain <34248798+scarsPain@users.noreply.github.com>
2022-01-02 15:02:27 +0100

    test

---
+++ src/main/java/org/example/test/AnotherClass.java
@@ -0,0 +1,9 @@
+package org.example.test;
+
+import static java.lang.System.*;
+
+public class AnotherClass {
+    public static void main(String[] args){
+        err.println("test 3");
+    }
+}
--- src/test/java/org/example/testclass/TestClass.java
+++ src/test/java/org/example/testclass/TestClass.java
@@ -1,5 +1,9 @@
 package org.example.testclass;
 
-public class TestClass {
+import static java.lang.System.err;
 
+public class TestClass {
+    public static void main(String[] args){
+        err.println("test 2");
+    }
 }
